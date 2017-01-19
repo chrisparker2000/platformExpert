12972f3cc8b586cf242ffa8182b4be3f2c6de986
Chris Parker <chrisparker2000@yahoo.com>
2017-01-19 11:08:30 -0500

    updated project to work in xcode 8.2, fixed segfault by getting serial-number if no parameter is on cmd line

--- main.c
+++ main.c
@@ -5,13 +5,27 @@
 int main (int argc, const char * argv[]) {
 	CFDataRef data;
 	
+    char * arg;
+    if (argc == 1)
+    {
+        char *c_msg = "Called without arguments, looking up serial-number";
+        CFStringRef cf_msg = CFStringCreateWithCString(0, c_msg, kCFStringEncodingUTF8);
+        CFShow(cf_msg);
+        
+        arg = "serial-number";
+    }
+    else
+    {
+        arg = (char *) argv[1];
+    }
+    
 	io_service_t platformExpert = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("IOPlatformExpertDevice"));
 	if (platformExpert)
 	{
 		data = IORegistryEntryCreateCFProperty(platformExpert,
 											   CFStringCreateWithCString(
 																		 NULL,
-																		 argv[1],
+																		 arg,
 																		 kCFStringEncodingUTF8),
 											   kCFAllocatorDefault, 0);
 	}
